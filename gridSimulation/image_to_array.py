2d01ab7115313f962ec0a20804a04b74ceba0c39
asrii00 <t2rias00@students.oamk.fi>
2025-06-06 22:09:17 +0300

    Grid simulation start

---
+++ gridSimulation/image_to_array.py
@@ -0,0 +1,15 @@
+from PIL import Image
+import numpy as np
+
+# Load greyscale image
+img = Image.open('predation_small.png').convert('L')  # 'L' = greyscale
+
+# Normalize pixel values to 0..1
+arr = np.array(img) / 255.0
+
+# Write to JS file
+with open('predationMapSmall.js', 'w') as f:
+    f.write('export const predationMapSmall = [\n')
+    for row in arr:
+        f.write('  [' + ', '.join(f'{v:.4f}' for v in row) + '],\n')
+    f.write('];\n')
