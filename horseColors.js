a77a414dfeee58600393d367b2aab9b1eff1e7f6
asrii00 <t2rias00@students.oamk.fi>
2025-07-16 13:40:54 +0300

    Create horseColors.js

---
+++ horseColors.js
@@ -0,0 +1,488 @@
+const genotypeMap= {
+    "EEAACCDD": {
+        "desc": "valkovoikko, hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAACCDd": {
+        "desc": "valkovoikko, hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAACCdd": {
+        "desc": "valkovoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAACcDD": {
+        "desc": "valkohallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAACcDd": {
+        "desc": "valkohallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAACcdd": {
+        "desc": "ruunivoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAAccDD": {
+        "desc": "ruunihallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAAccDd": {
+        "desc": "ruunikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAAccdd": {
+        "desc": "",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAaCCDD": {
+        "desc": "valkovoikko, hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAaCCDd": {
+        "desc": "valkovoikko, hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAaCCdd": {
+        "desc": "valkovoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAaCcDD": {
+        "desc": "valkohallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAaCcDd": {
+        "desc": "valkohallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAaCcdd": {
+        "desc": "ruunivoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAaccDD": {
+        "desc": "ruunihallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAaccDd": {
+        "desc": "ruunihallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEAaccdd": {
+        "desc": "ruunikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEaaCCDD": {
+        "desc": "valkovoikko, hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEaaCCDd": {
+        "desc": "valkovoikko, hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEaaCCdd": {
+        "desc": "valkovoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEaaCcDD": {
+        "desc": "mustanvoikko + hallakko", //?
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEaaCcDd": {
+        "desc": "mustanvoikko + hallakko", //?
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEaaCcdd": {
+        "desc": "mustanvoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEaaccDD": {
+        "desc": "hiirakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEaaccDd": {
+        "desc": "hiirakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EEaaccdd": {
+        "desc": "musta",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAACCDD": {
+        "desc": "valkovoikko + hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAACCDd": {
+        "desc": "valkovoikko + hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAACCdd": {
+        "desc": "valkovoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAACcDD": {
+        "desc": "valkohallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAACcDd": {
+        "desc": "valkohallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAACcdd": {
+        "desc": "ruunivoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAAccDD": {
+        "desc": "ruunihallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAAccDd": {
+        "desc": "ruunihallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAAccdd": {
+        "desc": "ruunikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAaCCDD": {
+        "desc": "valkovoikko, hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAaCCDd": {
+        "desc": "valkovoikko, hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAaCCdd": {
+        "desc": "valkovoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAaCcDD": {
+        "desc": "valkohallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAaCcDd": {
+        "desc": "valkohallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAaCcdd": {
+        "desc": "ruunivoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAaccDD": {
+        "desc": "ruunihallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAaccDd": {
+        "desc": "ruunihallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeAaccdd": {
+        "desc": "ruunikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeaaCCDD": {
+        "desc": "valkovoikko + hallakko", 
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeaaCCDd": {
+        "desc": "valkovoikko + hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeaaCCdd": {
+        "desc": "valkovoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeaaCcDD": {
+        "desc": "hiirakko + mustanvoikko", //?
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeaaCcDd": {
+        "desc": "hiirakko + mustanvoikko", //?
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeaaCcdd": {
+        "desc": "mustanvoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeaaccDD": {
+        "desc": "hiirakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "EeaaccDd": {
+        "desc": "hiirakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "Eeaaccdd": {
+        "desc": "musta",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAACCDD": {
+        "desc": "valkovoikko, hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAACCDd": {
+        "desc": "valkovoikko, hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAACCdd": {
+        "desc": "valkovoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAACcDD": {
+        "desc": "keltahallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAACcDd": {
+        "desc": "keltahallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAACcdd": {
+        "desc": "voikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAAccDD": {
+        "desc": "punahallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAAccDd": {
+        "desc": "punahallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAAccdd": {
+        "desc": "rautias",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAaCCDD": {
+        "desc": "valkovoikko, hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAaCCDd": {
+        "desc": "valkovoikko, hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAaCCdd": {
+        "desc": "valkovoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAaCcDD": {
+        "desc": "keltahallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAaCcDd": {
+        "desc": "keltahallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAaCcdd": {
+        "desc": "voikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAaccDD": {
+        "desc": "punahallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAaccDd": {
+        "desc": "punahallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeAaccdd": {
+        "desc": "rautias",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeaaCCDD": {
+        "desc": "valkovoikko, hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeaaCCDd": {
+        "desc": "valkovoikko, hallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeaaCCdd": {
+        "desc": "valkovoikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeaaCcDD": {
+        "desc": "keltahallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeaaCcDd": {
+        "desc": "keltahallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeaaCcdd": {
+        "desc": "voikko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeaaccDD": {
+        "desc": "punahallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeaaccDd": {
+        "desc": "punahallakko",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    },
+    "eeaaccdd": {
+        "desc": "rautias, piilomusta",
+        "baseColor": "",
+        "maneColor": "",
+        "stripeColor": null
+    }
+}
