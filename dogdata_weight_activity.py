478106b50392c7e117c16e52a516fc432b7e6605
asrii00 <t2rias00@students.oamk.fi>
2025-07-24 21:17:10 +0300

    temperament data exploration, sheltie data processing

--- dogdata_weight_activity.py
+++ dogdata_weight_activity.py
@@ -5,7 +5,8 @@ import json
 
 # === Load and clean data ===
 df = pd.read_excel("data-for-publication_edited_random.xlsx")
-df = df.head(2000)
+df = df.head(5000)
+df = df.tail(2000)
 df.columns = df.columns.str.strip()
 
 # === Columns to keep ===
